abf0104a19ad4f71ffbb212147d7d7ab79e41e9d
Kichan <rlcks01537@gmail.com>
2023-05-25 17:27:16 +0900

    typechain

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,49 @@
-type Words = {
-    [key:string]:string
+import crypto from "crypto";
+
+interface BlockShape {
+  hash:string;
+  prevHash:string;
+  height:number;
+  data:string;
 }
 
-class Dict{
-    private words:Words
-    constructor(){
-        this.words={}
-    }
-    add(word:Word){
-        if(this.words[word.term]===undefined){
-            this.words[word.term]=word.def
-        }
-        return undefined
-    }
-    get(word:Word){
-        return this.words[word.def]
-    }
-    delete(word:Word){
-        if(this.words[word.term]){
-            delete this.words[word.term]
-        }
-    }
-    update(word:Word){
-        if (this.words[word.term] !== undefined) {
-            this.words[word.term] = word.def
-        }
-    }
-    showAll(){
-        Object.keys(this.words).forEach((term)=>{
-            const printTerm=this.words[term];
-            console.log(`${printTerm}`);
-        })
-    }
-    count():number{
-            return Object.keys(this.words).length;
-    }
-    upsert(word:Word){
-        if(this.words[word.term]===undefined){
-            this.add(word)
-        }else{
-            this.add(word)
-        }
-    }
-    exist(term:string):boolean{
-        return this.words[term]!==undefined;
-    }
-    bulkAdd(wordList:Word[]){
-        wordList.forEach((word)=>{
-            this.add(word);
-        })
-    }
-    bulkDelete(termList: string[]) {
-        termList.forEach((term) => {
-        const word = new Word(term, "");
-        this.delete(word);
-        });
-    }
+class Block implements BlockShape{
+  public hash: string;
+  constructor(
+    public prevHash:string,
+    public height:number,
+    public data:string
+  ) {
+    this.hash=Block.calculateHash(prevHash,height,data);
+  }
+  static calculateHash(prevHash:string,height:number,data:string){
+    const toHash=`${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
 }
 
- class Word {
-    constructor(
-        public term: string,
-        public def:string
-    ){}
- }
+class BlockChain {
+  private blocks:Block[]
+  constructor(){
+    this.blocks=[];
+  }
+  private getPrevHash(){
+    if(this.blocks.length===0) return ""
+    return this.blocks[this.blocks.length-1].hash
+  }
+  public addBlock(data:string){
+    const block=new Block(this.getPrevHash(),this.blocks.length+1,data);
+    this.blocks.push(block);
+  }
+  public getBlocks(){
+    return [...this.blocks];
+  }
+}
 
-const dict=new Dict
-const wordHamburger=new Word("햄버거","고기를 갈아 넣고 버무린 뒤 구워낸 햄버그 스테이크(패티)를 빵에 싸먹는다.")
-const added=dict.add(wordHamburger); // add word
-const deff=dict.get(wordHamburger);
-const deleted=dict.delete(wordHamburger); //delete word
-const wordUpdate=new Word("햄버거","치즈와 소스 각종 토핑을 올려 화덕에 구워 먹는 이탈리아의 전통요리");
-const updated=dict.update(wordUpdate);
-const printWords=dict.showAll();
-const showCount=dict.count();
-const updateAndinsert=dict.upsert(new Word("치킨","닭을 기름에 튀겨서 만든요리 아주 맛이 좋음"));
-const inserAndUpdate=dict.upsert(new Word("햄버거","고기를 갈아 넣고 버무린 뒤 구워낸 햄버그 스테이크(패티)를 빵에 싸먹는다."));
-const existWord=dict.exist("치킨");
-const noExistWord=dict.exist("오징어튀김");
-const wordList=[
-    {term:"라면",def:"해산물 또는 육고기를 넣어 삶은 육수를 고명과 같이 먹는 일본식 면 요리"},
-    {term:"돼지국밥",def:"돼지 살코기와 특수부위를 삶은 육수를 밥과 같이 먹는 한국식 국물 요리"},
-    ];
-dict.bulkAdd(wordList);
-dict.showAll();
-dict.bulkDelete(["라면","돼지국밥"]);
-dict.showAll();
+const blockchain =new BlockChain();
+
+blockchain.addBlock("First One");
+blockchain.addBlock("Second One");
+blockchain.addBlock("Third One");
+
+console.log(blockchain.getBlocks());
